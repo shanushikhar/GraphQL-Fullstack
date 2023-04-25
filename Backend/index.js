ec84ac66528695c7d4847bdfa02187cdf5c05114
Shanu Shikhar <thisisshanushikhar@gmail.com>
2023-04-25 19:10:50 +0530

    Added way to query nested API

--- Backend/index.js
+++ Backend/index.js
@@ -7,3 +7,36 @@ const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
   console.log(`server is running on :- ${url}`);
 });
+
+/**
+ getting the data like => 
+ query getUsers {
+  users {
+    id
+    name
+    age
+    email
+    phone
+    __typename
+    company {
+      name
+      catchPhrase
+      __typename
+    }
+    address {
+      city
+      street
+      zipcode
+      suite
+      __typename
+      geo {
+        lat
+        lng
+        __typename
+      }
+    }
+    website
+  }
+}
+
+ */
