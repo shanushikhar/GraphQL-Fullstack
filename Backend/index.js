8f973773723ed053db7fe67ff6d84f9d7870d9e0
Shanu Shikhar <thisisshanushikhar@gmail.com>
2023-04-25 21:35:27 +0530

    Added more ways to query DB

--- Backend/index.js
+++ Backend/index.js
@@ -10,6 +10,7 @@ server.listen().then(({ url }) => {
 
 /**
  getting the data like => 
+ 1> query all users
  query getUsers {
   users {
     id
@@ -39,4 +40,70 @@ server.listen().then(({ url }) => {
   }
 }
 
+2> query all users
+query getUsers {
+  users {
+    id
+    name
+    age 
+    website
+    friends {
+      id
+      age
+      email
+      website
+      username
+    }
+  }
+}
+
+3> query by id 
+query getUser($userId: ID!){
+  user(id: $userId) {
+    id
+    age
+    name
+    email
+  }
+}
+
+in variable => 
+{
+  "userId": 4
+}
+
+4> query by name
+query user($name: String!){
+  userByName(name: $name) {
+    age
+    name
+    address {
+      geo {
+        lat
+        lng
+      }
+    }
+  }
+}
+in variable => 
+{
+  "name": "Ervin Howell"
+}
+
+5> fetching data form another table
+query user($name: String!){
+  userByName(name: $name) {
+    age
+    name
+    favoritesMovie {
+      name
+      released
+      yor
+    }
+  }
+}
+in variable => 
+{
+  "name": "Ervin Howell"
+}
  */
